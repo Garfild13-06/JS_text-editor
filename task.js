3cc2ea86c797916181c73a2e20e9cfc28788c168
garfild <garfild13-06@yandex.com>
2024-04-17 09:56:00 +0300

    Применение рекомендаций

--- task.js
+++ task.js
@@ -1,13 +1,11 @@
 const textarea = document.querySelector('textarea');
 const clearButton = document.querySelector('button');
 const textareaValue = localStorage.getItem('textarea');
-if (textareaValue != null) {
-    textarea.value = textareaValue;
-}
+textarea.value = textareaValue;
 
 clearButton.addEventListener('click', () => {
     textarea.value = '';
-    localStorage.clear()
+    localStorage.removeItem('textarea');
 })
 
 textarea.addEventListener('input', () => {
